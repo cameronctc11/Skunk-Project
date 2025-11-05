285c46f54e226eaed2a3bea1746af69da2a20a6b
cameronmckenzie <cameronctc11@gmail.com>
2025-11-05 13:24:16 -0700

    Initial Commit

---
+++ Skunk Project/Skunk_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skunk_ProjectApp.swift
+//  Skunk Project
+//
+//  Created by McKenzie, Cameron - Student on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Skunk_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Skunk ProjectTests/Skunk_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Skunk_ProjectTests.swift
+//  Skunk ProjectTests
+//
+//  Created by McKenzie, Cameron - Student on 11/5/25.
+//
+
+import Testing
+@testable import Skunk_Project
+
+struct Skunk_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
