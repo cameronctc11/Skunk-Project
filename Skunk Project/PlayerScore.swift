cbec24c9e97cb15a304d267cf2af898ccdb1ca8a
cameronmckenzie <cameronctc11@gmail.com>
2025-11-10 22:46:19 -0700

    finished skunk app

---
+++ Skunk Project/PlayerScore.swift
@@ -0,0 +1,8 @@
+//
+//  PlayerScore.swift
+//  Skunk Project
+//
+//  Created by McKenzie, Cameron - Student on 11/7/25.
+//
+
+import Foundation
---
+++ Skunk Project/RulesView.swift
@@ -0,0 +1,18 @@
+//
+//  RulesView.swift
+//  Skunk Project
+//
+//  Created by McKenzie, Cameron - Student on 11/5/25.
+//
+
+import SwiftUI
+
+struct RulesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RulesView()
+}
